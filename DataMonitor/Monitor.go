0fefbebd35bd96c5b0195e23aa532ef9bdd004a4
Jokubas Akramas <j.akramas@gmail.com>
2020-11-08 03:59:45 +0200

    Worker pasiekimas duomenu is duomenu strukturos

--- DataMonitor/Monitor.go
+++ DataMonitor/Monitor.go
@@ -3,61 +3,17 @@ package DataMonitor
 import (
 	IT "../Item"
 	"fmt"
-	"strconv"
 	"sync"
 )
-type SortedResultMonitor struct {
-	container []IT.ItemWithResult
-	Length    int
-	mutex     *sync.Mutex
-}
-type DataMonitor struct {
-	container   []IT.Item
-	currentSize int
-	length      int
-	removed     int
-	workDone    bool
-	mutex       *sync.Mutex
-	cond    	*sync.Cond
-}
 
-func (monitor *DataMonitor) ToString() string {
-	return fmt.Sprintf(
-		"container:\n%s, currentSize: %d," +
-			"length: %d, removed: %d,  workdone: %v \n",
-			monitor.ContainerString(), monitor.currentSize,
-			monitor.length, monitor.removed, monitor.workDone)
-}
-
-func (monitor *DataMonitor) ContainerString() string{
-	data := ""
-	for _, element := range monitor.container{
-		data += element.ToString() + "\n"
-	}
-	return data
-}
-
-func InitializeDM(size int) DataMonitor {
-	var container = make([]IT.Item, size/3)
-	var mutex = sync.Mutex{}
-	var cond = sync.NewCond(&mutex)
-	var dataMonitor = DataMonitor{container: container, workDone: false, length: size, removed: 0, mutex: &mutex, cond: cond}
-	return dataMonitor
-}
-
-func InitializeSDM(size int) SortedResultMonitor {
-	var container = make([]IT.ItemWithResult, size)
-	var mutex = sync.Mutex{}
-	var dataMonitor = SortedResultMonitor{container: container, Length: 0, mutex: &mutex}
-	return dataMonitor
-}
-
-func ProvideItems(items IT.Items) {
+func ProvideItems(items IT.Items, writeChan chan<- IT.Item, writeFlag chan<- int, end chan<- int) {
 	for _, item := range items.Items {
-		monitor.addItem(item)
+		writeFlag <- 1
+		writeChan <- item
 	}
+	end <- 1
 }
-
+/*
 func (resultMonitor *SortedResultMonitor) addItemSorted(item IT.ItemWithResult){
 	//surikiuoti
 	resultMonitor.mutex.Lock()
@@ -78,8 +34,8 @@ func (resultMonitor *SortedResultMonitor) addItemSorted(item IT.ItemWithResult){
 		}
 	}
 	resultMonitor.mutex.Unlock()
-}
-
+}*/
+/*
 func (resultMonitor *SortedResultMonitor) GetItems() []IT.ItemWithResult{
 	var container = make([]IT.ItemWithResult, len(resultMonitor.container))
 	x := 0
@@ -96,21 +52,68 @@ func (resultMonitor *SortedResultMonitor) GetItems() []IT.ItemWithResult{
 		finalContainer[i] = container[i]
 	}
 	return finalContainer
+}*/
+
+func DataProcess(group *sync.WaitGroup, size int, readChan <-chan IT.Item, readFlag <-chan int, writeChan chan<- IT.Item, writeFlag <-chan int, end <-chan int){
+	defer group.Done()
+	container := make([]IT.Item, size/3)
+	count := 0
+	for {
+		if count >= len(container)-1{
+			//fmt.Println("cia2")
+			//Pilnas - nepriimti rasanciu (tik salinancius)
+			<-writeFlag
+			count--
+			writeChan <- container[count]
+		} else if count <= 0{
+			//fmt.Println("cia")
+			//Tuscias - nepriimti salinanciu (tik rasancius)
+			<- readFlag
+			container[count] = <- readChan
+			count++
+		} else {
+			//fmt.Println("cia3")
+			select {
+			case <- readFlag:
+				//fmt.Println("opa1")
+				container[count] = <- readChan
+				count++
+			case <- writeFlag:
+				//fmt.Println("opa2")
+				count--
+				writeChan <- container[count]
+			case <- end:
+				break
+			}
+
+		}
+
+		/*for u := 0; u < len(container); u++ {
+			fmt.Println(container[u])
+		}*/
+		//fmt.Println(count)
+		//fmt.Println()
+
+
+	}
+	//fmt.Println("END OF STREAM")
 }
 
-func DataProcess(group *sync.WaitGroup, count int, readChan <-chan IT.Item, writeChan chan<- IT.Item){
+func ResultProcess(group *sync.WaitGroup, size int, readChan <-chan IT.ItemWithResult, writeChan chan<- []IT.ItemWithResult){
 	defer group.Done()
-	dataMonitor := InitializeDM(count)
+	//resultMonitor := InitializeSDM(count)
 }
 
-func ResultProcess(group *sync.WaitGroup, count int, readChan <-chan IT.ItemWithResult, writeChan chan<- []IT.ItemWithResult){
+func WorkProcess(group *sync.WaitGroup, readChan <-chan IT.Item, readFlag chan<- int, writeChan chan IT.ItemWithResult, end <-chan int) {
 	defer group.Done()
-	resultMonitor := InitializeSDM(count)
+	for {
+		readFlag <- 1
+		item := <-readChan
+		fmt.Println(item.ToString())
+	}
 
-}
-
-func WorkProcess(group *sync.WaitGroup, readChan <-chan IT.Item, writeChan chan<- IT.ItemWithResult) {
-	defer group.Done()
+	//fmt.Println("END OF WORK")
+	/*
 	done := false
 	for !done {
 		item, doneNow, last := monitor.removeItem()
@@ -123,9 +126,9 @@ func WorkProcess(group *sync.WaitGroup, readChan <-chan IT.Item, writeChan chan<
 		if doneNow || last {
 			done = true
 		}
-	}
+	}*/
 }
-
+/*
 func (monitor *DataMonitor) addItem(item IT.Item) {
 	monitor.mutex.Lock()
 	for monitor.currentSize > len(monitor.container)-1{
@@ -159,4 +162,4 @@ func (monitor *DataMonitor) removeItem() (IT.Item, bool, bool) {
 	monitor.cond.Broadcast()
 	monitor.mutex.Unlock()
 	return item, workDone, last
-}
+}*/
