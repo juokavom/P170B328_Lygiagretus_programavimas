1f51702b409917fa35121aee4b84967e743b54ce
Jokūbas Akramas <j.akramas@gmail.com>
2020-12-28 19:57:08 +0200

    Implemented queue instead of stack

--- Egzaminas/main.js
+++ Egzaminas/main.js
@@ -4,84 +4,55 @@ const system = start();
 const delay = (time) => new Promise((res) => setTimeout(res, time));
 
 const array = [...Array(3).keys()];
-const names = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
-//const names = ['First'];
+const data = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
 
 
 const worker = [];
-const bottleneck = [];
 
 
 array.forEach(i => {
     worker.push(
         spawnStateless(system, async (msg, ctx) =>  {
-            //console.log(ctx.name, ', message: ', msg.name, ', working...');
+            //console.log(ctx.name, ', message: ', msg.item, ', working...');
             await delay(500);
             //console.log(ctx.name, ', sending back to balancer...');
-            dispatch(balancer, { name: msg.name, flag: 2, index: i });
+            dispatch(balancer, { item: msg.item, flag: 2, index: i });
           }, `worker-${i}`));
   });
 
 console.log(worker.length);
 
 
+const bottleneck = [];
 const balancer = spawnStateless(system, async (msg, ctx) =>  {
 
+    //Send to worker
     if(msg.flag == 1){
         if(array.length < 1){
-            //console.log('No free actor workers, adding to stack for later ', msg.name);
-            bottleneck.push(msg.name);     
+            //console.log('No free actor workers, adding to queue for later ', msg.name);
+            bottleneck.push(msg.item);     
         } else {
-            send(msg.name);
+            send(msg.item);
         }
     }
+    //Receive from worker
     if(msg.flag == 2){
-        console.log('Received from worker-', msg.index, ', value : ', msg.name);
+        console.log('Received from worker-', msg.index, ', value : ', msg.item);
         array.push(msg.index);
         if(bottleneck.length > 0){    
-            send(bottleneck.pop());
+            send(bottleneck.shift());
         }
     }
 
-    function send(name){
+    function send(item){
         //console.log('Balancer, value : ', name, '. Sending to worker.');
-        dispatch(worker[array.pop()], { name: name});
+        dispatch(worker[array.shift()], { item: item});
     }
 
-  }, 'balancer');
+}, 'balancer');
 
 
-
-
-  names.forEach(element => {
+data.forEach(element => {
     //console.log(element);
-    dispatch(balancer, { name: element, flag: 1 });
+    dispatch(balancer, { item: element, flag: 1 });
 });
-
-/*
-const ping = spawnStateless(system, async (msg, ctx) =>  {
-  //console.log(msg.value);
-  // ping: Pong is a little slow. So I'm giving myself a little handicap :P
-  await delay(500);
-  //dispatch(msg.sender, { value: ctx.name, sender: ctx.self });
-  dispatch(msg.sender, { value: msg.value, sender: ctx.self });
-}, 'ping');
-
-const pong = spawnStateless(system, (msg, ctx) =>  {
-  console.log(msg.value);
-  //dispatch(msg.sender, { value: ctx.name, sender: ctx.self });
-}, 'pong');
-
-
-const map1 = array.map(el => el = el * 2);
-
-map1.forEach(element => {
-    console.log(element);
-});
-
-
-dispatch(ping, { value: 'begin1', sender:pong });
-dispatch(ping, { value: 'begin2', sender:pong });
-dispatch(ping, { value: 'begin3', sender:pong });
-dispatch(ping, { value: 'begin4', sender:pong });
-*/
