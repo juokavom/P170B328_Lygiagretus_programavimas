5211afdea94f142dc67ade2b0e72c25bd474965c
Jokūbas Akramas <j.akramas@gmail.com>
2020-12-28 23:19:46 +0200

    Added Data files and remade bottleneck queue to actors mailbox

--- Egzaminas/main.js
+++ Egzaminas/main.js
@@ -1,8 +1,6 @@
 'use strict';
-
 const { start, dispatch, stop, spawnStateless } = require('nact');
 const system = start();
-
 const delay = (time) => new Promise((res) => setTimeout(res, time));
 
 const array = [...Array(7).keys()];
@@ -12,43 +10,38 @@ const worker = [];
 array.forEach(i => {
     worker.push(
         spawnStateless(system, async (msg, ctx) =>  {
-            //console.log(ctx.name, ', message: ', msg.item, ', working...');
-            //await delay(500);
-            //console.log(ctx.name, ', sending back to balancer...');
+            console.log(ctx.name, ', message: ', msg.item, ', working...');
+
+            // var myBuffer = [];
+            // var buffer = new Buffer(msg.item.title, 'utf16le');
+            // for (var i = 0; i < buffer.length; i++) {
+            //     myBuffer.push(buffer[i]);
+            // }     
+            await delay(500);
+            console.log(ctx.name, ', sending back to balancer...');
             dispatch(balancer, { item: msg.item, flag: 2, index: i });
           }, `worker-${i}`));
 });
 
 const bottleneck = [];
 const balancer = spawnStateless(system, async (msg, ctx) =>  {
-
-    //Send to worker
     if(msg.flag == 1){
-        if(array.length < 1){
-            //console.log('No free actor workers, adding to queue for later ', msg.name);
-            bottleneck.push(msg.item);     
-        } else {
-            send(msg.item);
-        }
+        send(msg.item);
     }
-    //Receive from worker
     if(msg.flag == 2){
-        console.log('Received from worker-', msg.index, ', value : ', msg.item);
-        array.push(msg.index);
-        if(bottleneck.length > 0){    
-            send(bottleneck.shift());
-        }
+        console.log('Received from worker-',msg.index, ', value : ', msg.item);
     }
 
     function send(item){
-        //console.log('Balancer, value : ', name, '. Sending to worker.');
-        dispatch(worker[array.shift()], { item: item});
+        console.log('Balancer, value : ', item, '. Sending to worker.');
+        const index = array.shift();
+        dispatch(worker[index], { item: item});
+        array.push(index);
     }
 
 }, 'balancer');
 
 
 data['items'].forEach(element => {
-    //console.log(element);
     dispatch(balancer, { item: element, flag: 1 });
 });
