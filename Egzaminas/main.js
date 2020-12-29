29863f31f0e3ca255b3ca56e89e42381060baea1
Jokūbas Akramas <j.akramas@gmail.com>
2020-12-29 17:54:22 +0200

    Printing txt table

--- Egzaminas/main.js
+++ Egzaminas/main.js
@@ -9,48 +9,56 @@ const data = require('./Data/IFF8-12_AkramasJ_L1_dat_1.json');
 const worker = [];
 array.forEach(i => {
     worker.push(
-        spawnStateless(system, async (msg, ctx) =>  {
+        spawnStateless(system, async (msg, ctx) => {
             //console.log(ctx.name, ', message: ', msg.item, ', working...');
             msg.item.result = ([...Buffer.from(msg.item.title)].reduce((sum, i) => sum + i, 0) ^ msg.item.quantity) * msg.item.price;
-            if(msg.item.result %1 > 0.5){            
+            if (msg.item.result % 1 > 0.5) {
                 //console.log(ctx.name, ', sending back to balancer...');
-                dispatch(balancer, { item: msg.item, flag: 2, worker: i});
+                dispatch(balancer, { item: msg.item, flag: 2, worker: i });
             }
-          }, `worker-${i}`));
+        }, `worker-${i}`));
 });
 
 const repo = [];
-const repository = spawnStateless(system, async (msg, ctx) =>  {
-    if(msg.flag == 1){
+const repository = spawnStateless(system, async (msg, ctx) => {
+    if (msg.flag == 1) {
         repo.push(msg.item);
         repo.sort((a, b) => a.result - b.result);
     }
-    if(msg.flag == 2){
+    if (msg.flag == 2) {
         dispatch(balancer, { items: repo, flag: 4 });
     }
 }, 'repository');
 
-const printer = spawnStateless(system, async (msg, ctx) =>  {
+const printer = spawnStateless(system, async (msg, ctx) => {
     //console.log(msg.items);
-    msg.items.forEach(i => console.log(i.title, ' ', i.result));
+    const output = '-------------------------------------------------\n|Title               |Quantity|Price  |Result   |' +
+        '\n-------------------------------------------------\n'.concat(msg.items.reduce((output, i) => output.concat('|', i.title.padEnd(20), '|',
+            i.quantity.toString().padEnd(8), '|', i.price.toString().padEnd(7), '|', i.result.toFixed(2).toString().padEnd(9), '|\n'), ''),
+            '-------------------------------------------------');
+    const fs = require('fs');
+    fs.writeFile('./Data/IFF8-12_AkramasJ_Egzaminas_rez.txt', output, function (err) {
+        if (err) throw err;
+        //console.log('Printed!');
+    });
 }, 'printer');
 
-const balancer = spawnStateless(system, async (msg, ctx) =>  {
-    if(msg.flag == 1){
+const balancer = spawnStateless(system, async (msg, ctx) => {
+    if (msg.flag == 1) {
         //console.log('Balancer, value : ', msg.item, '. Sending to worker.');
         const index = array.shift();
-        dispatch(worker[index], { item: msg.item});
+        dispatch(worker[index], { item: msg.item });
         array.push(index);
     }
-    if(msg.flag == 2){
+    if (msg.flag == 2) {
         //console.log('Received from worker-',msg.worker, ', value : ', msg.item, ', result: ', msg.result);
-        dispatch(repository, {item: msg.item, flag: 1});
+        dispatch(repository, { item: msg.item, flag: 1 });
     }
-    if(msg.flag == 3){
-        dispatch(repository, {flag: 2});
+    if (msg.flag == 3) {
+        dispatch(repository, { flag: 2 });
     }
-    if(msg.flag == 4){
-        dispatch(printer, {items: msg.items});
+    if (msg.flag == 4) {
+        dispatch(printer, { items: msg.items });
     }
 
 }, 'balancer');
